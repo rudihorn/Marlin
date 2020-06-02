db32e123ed1ed4166281268ebbc7f2ebf1320ea7
Rudi Horn <dyn-git@rudi-horn.de>
2020-06-02 23:14:06 +0100

    delay is ready until sd card is initialised

--- Marlin/src/HAL/STM32/msc_sd.cpp
+++ Marlin/src/HAL/STM32/msc_sd.cpp
@@ -7,19 +7,18 @@
   #include "usbd_msc.h"
   #include "usbd_msc_storage_if.h"
   #include "USB.h"
-  
+
   // Sd2Card uses 512 bytes
   #define BLOCK_SIZE 512U
 
   Sd2Card * pCard = nullptr;
-
 
   static int8_t MSC_GetCapacity(uint8_t lun, uint32_t *block_num, uint16_t *block_size) {
       *block_num = pCard->cardSize();
       *block_size = BLOCK_SIZE;
       return USBD_OK;
   }
-  
+
   static int8_t MSC_Write(uint8_t lun, uint8_t *buf, uint32_t blk_addr, uint16_t blk_len) {
     if (!pCard->writeStart(blk_addr, blk_len))
       return false;
@@ -47,21 +46,26 @@
     pCard->readStop();
     return USBD_OK;
   }
-  
+
   static int8_t MSC_GetMaxLun(void) {
     return 0; // only one storage logical unit
   }
 
   static int8_t MSC_IsReady(uint8_t lun) {
-    return 0;
+    if (pCard->cardSize() > 0) {
+      // tell the host the sd card has been initialised
+      return 0;
+    } else {
+      // signal to the host that the SD card is still initialising
+      return 1;
+    }
   }
-   
+
   static int8_t MSC_IsWriteProtected(uint8_t lun) {
     return 0;
   }
-  
+
   static int8_t MSC_Init(uint8_t lun) {
-    pCard = &card.getSd2Card();
     return 0;
   }
 
@@ -80,6 +84,7 @@
 
 void MSC_SD_Init() {
   #if SD_CONNECTION_IS(ONBOARD)
+    pCard = &card.getSd2Card();
     USBDevice.register_msc(&USBD_MSC_SD_fops);
   #endif
 }
